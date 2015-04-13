ba126152212e20f7979105e010282291daffe997
Matt Krebs <matt@murkser.ch>
2015-04-13 17:26:32 +0000

    cpm counter values not older than 1 minute

--- software/dotnet/GroundControl/GroundControl.Core/CpmCounter.cs
+++ software/dotnet/GroundControl/GroundControl.Core/CpmCounter.cs
@@ -15,7 +15,7 @@ namespace GroundControl.Core
         private DateTime ageLimit;
         private LinkedList<TelemetryData> cache;
 
-        private static readonly TimeSpan MaxAge = new TimeSpan(0, 1, 30);
+        private static readonly TimeSpan MaxAge = new TimeSpan(0, 1, 0);
 
         /// <summary>
         /// Gets the absolute count.
